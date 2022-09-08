50d7f65f2059b0341130c2e28ea52f7438118637
pengtao.geng <pengtao.geng>
2022-09-08 11:10:36 +0800

    增加code

---
+++ gpt-leetcode/src/main/java/Code_2280_MinimumLinesToRepresentALineChart/Solution.java
@@ -0,0 +1,15 @@
+package Code_2280_MinimumLinesToRepresentALineChart;
+
+/**
+ * @author pengtao.geng on 2022/9/8 11:08.
+ */
+public class Solution {
+
+	public int minimumLines(int[][] stockPrices) {
+		if (stockPrices == null || stockPrices.length < 2) {
+			return 0;
+		}
+
+		return 0;
+	}
+}
