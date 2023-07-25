a01f73511ae26cadf1a5c711d12494ea5c0005fb
pengtao.geng <pengtao.geng>
2023-07-25 22:02:17 +0800

    修改将数组和减半的最少操作次数

--- gpt-leetcode/src/main/java/com/gpengtao/leetcode/main/Q2208_MinimumOperationsToHalveArraySum.java
+++ gpt-leetcode/src/main/java/com/gpengtao/leetcode/main/Q2208_MinimumOperationsToHalveArraySum.java
@@ -1,15 +1,20 @@
 package com.gpengtao.leetcode.main;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
+ * 将数组和减半的最少操作次数
  * <a href="https://leetcode.cn/problems/minimum-operations-to-halve-array-sum/">...</a>
  *
  * @author pengtao.geng on 2023/7/25 10:18
  */
 public class Q2208_MinimumOperationsToHalveArraySum {
 
+	/**
+	 * pengtao.geng实现
+	 */
 	public int halveArray(int[] originNums) {
 		// copy
 		double[] nums = new double[originNums.length];
@@ -45,31 +50,37 @@ public class Q2208_MinimumOperationsToHalveArraySum {
 		return count;
 	}
 
+	/**
+	 * 官方实现，优先级队列
+	 */
 	public int halveArray2(int[] nums) {
-		PriorityQueue<Double> pq = new PriorityQueue<Double>((a, b) -> b.compareTo(a));
+		PriorityQueue<Double> queue = new PriorityQueue<>(Comparator.reverseOrder());
 		for (int num : nums) {
-			pq.offer((double) num);
+			queue.offer((double) num);
 		}
-		int res = 0;
+
 		double sum = 0;
 		for (int num : nums) {
 			sum += num;
 		}
+
+		int result = 0;
 		double sum2 = 0.0;
 		while (sum2 < sum / 2) {
-			double x = pq.poll();
-			System.out.println(x);
-			sum2 += x / 2;
-			pq.offer(x / 2);
-			res++;
+			Double maxValue = queue.poll();
+			assert maxValue != null;
+			sum2 += maxValue / 2;
+			queue.offer(maxValue / 2);
+			result++;
 		}
-		return res;
+		return result;
 	}
 
 	public static void main(String[] args) {
 		System.out.println(new Q2208_MinimumOperationsToHalveArraySum().halveArray(new int[]{5, 19, 8, 1}));
 		System.out.println(new Q2208_MinimumOperationsToHalveArraySum().halveArray(new int[]{3, 8, 20}));
-		System.out.println(new Q2208_MinimumOperationsToHalveArraySum().halveArray(new int[]{32, 98, 23, 14, 67, 40, 26, 9, 96, 96, 91, 76, 4, 40, 42, 2, 31, 13, 16, 37, 62, 2, 27, 25, 100, 94, 14, 3, 48, 56, 64, 59, 33, 10, 74, 47, 73, 72, 89, 69, 15, 79, 22, 18, 53, 62, 20, 9, 76, 64}));
-		System.out.println(new Q2208_MinimumOperationsToHalveArraySum().halveArray2(new int[]{32, 98, 23, 14, 67, 40, 26, 9, 96, 96, 91, 76, 4, 40, 42, 2, 31, 13, 16, 37, 62, 2, 27, 25, 100, 94, 14, 3, 48, 56, 64, 59, 33, 10, 74, 47, 73, 72, 89, 69, 15, 79, 22, 18, 53, 62, 20, 9, 76, 64}));
+		int[] list = {32, 98, 23, 14, 67, 40, 26, 9, 96, 96, 91, 76, 4, 40, 42, 2, 31, 13, 16, 37, 62, 2, 27, 25, 100, 94, 14, 3, 48, 56, 64, 59, 33, 10, 74, 47, 73, 72, 89, 69, 15, 79, 22, 18, 53, 62, 20, 9, 76, 64};
+		System.out.println(new Q2208_MinimumOperationsToHalveArraySum().halveArray(list));
+		System.out.println(new Q2208_MinimumOperationsToHalveArraySum().halveArray2(list));
 	}
 }
