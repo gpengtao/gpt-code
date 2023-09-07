d1c515752146af42004a273ca4fc9c9be92b7fde
pengtao.geng <pengtao.geng>
2023-09-07 20:48:51 +0800

    code 23

---
+++ gpt-leetcode/src/main/java/com/gpengtao/leetcode/linkedlist/Q06_23_MergeKSortedLists_合并K个升序链表.java
@@ -0,0 +1,15 @@
+package com.gpengtao.leetcode.linkedlist;
+
+import com.gpengtao.leetcode.linkedlist.node.ListNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/merge-k-sorted-lists/">...</a>
+ *
+ * @author pengtao.geng on 2023/9/7 20:45.
+ */
+public class Q06_23_MergeKSortedLists_合并K个升序链表 {
+
+	public ListNode mergeKLists(ListNode[] lists) {
+		return null;
+	}
+}
