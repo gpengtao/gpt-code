7458da8eee627ade12a8902815a2ff587f23056e
pengtao.geng <pengtao.geng>
2023-09-11 13:00:27 +0800

    code 86

---
+++ gpt-leetcode/src/main/java/com/gpengtao/leetcode/linkedlist/Q07_86_PartitionList_分隔链表.java
@@ -0,0 +1,41 @@
+package com.gpengtao.leetcode.linkedlist;
+
+import com.gpengtao.leetcode.linkedlist.node.ListNode;
+
+import static com.gpengtao.leetcode.linkedlist.node.ListNode.buildLinK;
+import static com.gpengtao.leetcode.linkedlist.node.ListNode.printList;
+
+/**
+ * <a href="https://leetcode.cn/problems/partition-list/">...</a>
+ *
+ * @author pengtao.geng on 2023/9/9 15:55.
+ */
+public class Q07_86_PartitionList_分隔链表 {
+
+	public ListNode partition(ListNode head, int x) {
+		ListNode list1 = new ListNode(-1);
+		ListNode list2 = new ListNode(-1);
+		ListNode p1 = list1;
+		ListNode p2 = list2;
+		while (head != null) {
+			if (head.val < x) {
+				p1.next = head;
+				p1 = p1.next;
+			} else {
+				p2.next = head;
+				p2 = p2.next;
+			}
+			head = head.next;
+		}
+		p1.next = list2.next;
+		p2.next = null;
+		return list1.next;
+	}
+
+	public static void main(String[] args) {
+		printList(new Q07_86_PartitionList_分隔链表().partition(buildLinK(2, 1), 2));
+		printList(new Q07_86_PartitionList_分隔链表().partition(buildLinK(1, 4, 3, 2, 5, 2), 3));
+		printList(new Q07_86_PartitionList_分隔链表().partition(buildLinK(4, 3, 2, 5, 2), 3));
+		printList(new Q07_86_PartitionList_分隔链表().partition(buildLinK(1, 4, 3, 0, 5, 2), 2));
+	}
+}
