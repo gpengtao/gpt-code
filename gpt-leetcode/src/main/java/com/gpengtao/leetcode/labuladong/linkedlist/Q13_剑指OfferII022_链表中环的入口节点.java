b7f4391081d8dad0220bfabe41afa6760bb3d857
pengtao.geng <pengtao.geng>
2023-09-21 20:48:02 +0800

    add

---
+++ gpt-leetcode/src/main/java/com/gpengtao/leetcode/labuladong/linkedlist/Q13_剑指OfferII022_链表中环的入口节点.java
@@ -0,0 +1,17 @@
+package com.gpengtao.leetcode.labuladong.linkedlist;
+
+import com.gpengtao.leetcode.labuladong.linkedlist.node.ListNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/c32eOV/">...</a>
+ *
+ * @author pengtao.geng on 2023/9/21 20:47.
+ * @see Q02_142_LinkedListCycleII_环形链表II
+ */
+public class Q13_剑指OfferII022_链表中环的入口节点 {
+
+	public ListNode detectCycle(ListNode head) {
+		// 相同
+		return null;
+	}
+}
