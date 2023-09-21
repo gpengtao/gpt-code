a374e371bba786d59945e3f61e595ab31d93c07a
pengtao.geng <pengtao.geng>
2023-09-21 20:54:01 +0800

    add

---
+++ gpt-leetcode/src/main/java/com/gpengtao/leetcode/labuladong/linkedlist/Q14_剑指OfferII023_两个链表的第一个重合节点.java
@@ -0,0 +1,17 @@
+package com.gpengtao.leetcode.labuladong.linkedlist;
+
+import com.gpengtao.leetcode.labuladong.linkedlist.node.ListNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/3u1WK4/">...</a>
+ *
+ * @author pengtao.geng on 2023/9/21 20:49.
+ * @see Q03_160_IntersectionOfTwoLinkedLists_相交链表
+ */
+public class Q14_剑指OfferII023_两个链表的第一个重合节点 {
+
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		// 相同
+		return null;
+	}
+}
---
+++ gpt-leetcode/src/main/java/com/gpengtao/leetcode/labuladong/linkedlist/Q15_剑指OfferII078_合并排序链表.java
@@ -0,0 +1,17 @@
+package com.gpengtao.leetcode.labuladong.linkedlist;
+
+import com.gpengtao.leetcode.labuladong.linkedlist.node.ListNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/vvXgSW/">...</a>
+ *
+ * @author pengtao.geng on 2023/9/21 20:53.
+ * @see Q06_23_MergeKSortedLists_合并K个升序链表
+ */
+public class Q15_剑指OfferII078_合并排序链表 {
+
+	public ListNode mergeKLists(ListNode[] lists) {
+		// 相同
+		return null;
+	}
+}
