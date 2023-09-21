f4e5308bce41384edabc89f558a301c9b7c696c1
pengtao.geng <pengtao.geng>
2023-09-21 20:36:53 +0800

    add q11

---
+++ gpt-leetcode/src/main/java/com/gpengtao/leetcode/labuladong/linkedlist/Q11_剑指Offer52_两个链表的第一个公共节点.java
@@ -0,0 +1,16 @@
+package com.gpengtao.leetcode.labuladong.linkedlist;
+
+import com.gpengtao.leetcode.labuladong.linkedlist.node.ListNode;
+
+/**
+ * <a href="https://leetcode.cn/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/">...</a>
+ *
+ * @author pengtao.geng on 2023/9/21 20:29.
+ * @see Q03_160_IntersectionOfTwoLinkedLists_相交链表
+ */
+public class Q11_剑指Offer52_两个链表的第一个公共节点 {
+
+	ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		return null;
+	}
+}
