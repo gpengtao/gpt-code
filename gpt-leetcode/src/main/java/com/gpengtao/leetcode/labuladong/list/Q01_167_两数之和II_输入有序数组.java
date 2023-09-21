9133b69a7ac16d3bc5d5eba5d7d2958d4487fe81
pengtao.geng <pengtao.geng>
2023-09-21 20:56:17 +0800

    修改

---
+++ gpt-leetcode/src/main/java/com/gpengtao/leetcode/labuladong/list/Q01_167_两数之和II_输入有序数组.java
@@ -0,0 +1,13 @@
+package com.gpengtao.leetcode.labuladong.list;
+
+/**
+ * <a href="https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/">...</a>
+ *
+ * @author pengtao.geng on 2023/9/21 20:55.
+ */
+public class Q01_167_两数之和II_输入有序数组 {
+
+	public int[] twoSum(int[] numbers, int target) {
+		return null;
+	}
+}
