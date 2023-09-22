d502c1f89e26ee29cb79fb7cc2477b45d7471cd0
pengtao.geng <pengtao.geng>
2023-09-22 10:31:38 +0800

    q01

--- gpt-leetcode/src/main/java/com/gpengtao/leetcode/labuladong/list/Q01_167_两数之和II_输入有序数组.java
+++ gpt-leetcode/src/main/java/com/gpengtao/leetcode/labuladong/list/Q01_167_两数之和II_输入有序数组.java
@@ -7,7 +7,38 @@ package com.gpengtao.leetcode.labuladong.list;
  */
 public class Q01_167_两数之和II_输入有序数组 {
 
+	/**
+	 * 解法1，两层for循环，暴力求解
+	 */
 	public int[] twoSum(int[] numbers, int target) {
+		int[] result = new int[2];
+		for (int i = 0; i < numbers.length; i++) {
+			// 注意j从i+1开始
+			for (int j = i + 1; j < numbers.length; j++) {
+				if (numbers[i] + numbers[j] == target) {
+					result[0] = i + 1;
+					result[1] = j + 1;
+					return result;
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * 解法2，双指针（leetcode评论区 omega 的杰作，碉堡了
+	 */
+	public int[] twoSum2(int[] numbers, int target) {
+		for (int i = 0, j = numbers.length - 1; i < j; ) {
+			int sum = numbers[i] + numbers[j];
+			if (sum == target) {
+				return new int[]{i + 1, j + 1};
+			} else if (sum > target) {
+				j--;
+			} else {
+				i++;
+			}
+		}
 		return null;
 	}
 }
