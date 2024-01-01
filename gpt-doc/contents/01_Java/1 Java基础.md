7188beb06d199186432ccfd1cbb01659045d8e00
pengtao.geng <pengtao.geng>
2024-01-01 22:26:56 +0800

    add

--- gpt-doc/contents/01_Java/1 Java基础.md
+++ gpt-doc/contents/01_Java/1 Java基础.md
@@ -1,6 +1,7 @@
 
-多线程
-1、java中有哪些方式实现多线程？
-（1）继承 Thread 类，即实现线程的 run() 方法，start()方法启动线程；
-（2）实现 Runnable 接口，即任务，交给 Thread 去运行；
-（3）用Timer，Timer里跑TimerTask；
+## 多线程
+### 1、java中有哪些方式实现多线程？
+- 继承 Thread 类，即实现线程的 run() 方法，start()方法启动线程；
+- 实现 Runnable 接口，即任务，交给 Thread 去运行；
+- 实现 FutureTask，里面指定了 Callable，用get可以拿到call的返回值，交给 Thread 去运行。FutureTask 本质也是 Runnable；
+- 用Timer，Timer里跑TimerTask；
--- gpt-java/src/test/java/com/gpengtao/java/thread/ThreadTest.java
+++ gpt-java/src/test/java/com/gpengtao/java/thread/ThreadTest.java
@@ -2,25 +2,37 @@ package com.gpengtao.java.thread;
 
 import org.junit.Test;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 /**
  * @author pengtao.geng on 2024/1/1 22:05.
  */
 public class ThreadTest {
 
 	@Test
-	public void test_impl_thread_1() {
+	public void test_implThread1_extends_thread() {
 		new MyThread().start();
 	}
 
 	@Test
-	public void test_impl_thread_2() {
+	public void test_implThread2_thread_use_run() {
 		new Thread(() -> System.out.println("hello")).start();
 	}
 
 	@Test
-	public void test_impl(){
-		Thread thread = new Thread();
-		thread.start();
+	public void test_implThread3_thread_use_future_task() throws ExecutionException, InterruptedException {
+		FutureTask<String> futureTask = new FutureTask<>(new Callable<String>() {
+			@Override
+			public String call() throws Exception {
+				Thread.sleep(1000);
+				return "hello";
+			}
+		});
+		new Thread(futureTask).start();
+		String ret = futureTask.get();
+		System.out.println(ret);
 	}
 
 	class MyThread extends Thread {
