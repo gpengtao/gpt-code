d189c4a2c0ffb1f0adba61f6de9008d4eb09f01c
pengtao.geng <pengtao.geng>
2024-05-09 21:28:14 +0800

    add test

--- gpt-scala/src/main/scala/HelloWorld.scala
+++ gpt-scala/src/main/scala/HelloWorld.scala
@@ -2,5 +2,24 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("hello world")
+    println(multiplier1(10))
+    println(multiplier2(10))
+    println(multiplier3(10))
   }
+
+  // 方法1，闭包
+  var factor = 10
+  val multiplier1 = (i: Int) => i * factor
+
+  // 方法2
+  def multiplier2(i: Int) = i + 10
+
+  // 方法3
+  def multiplier3(i: Int): Int = {
+    i * 10
+    return i * 20
+    i * 30
+  }
+
+
 }
