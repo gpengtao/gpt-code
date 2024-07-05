86e994e0069d50f833538dcc7e90a710835a98d8
pengtao.geng <pengtao.geng>
2024-07-05 13:08:00 +0800

    修改

--- gpt-learn/src/main/java/com/gpengtao/App.java
+++ gpt-learn/src/main/java/com/gpengtao/App.java
@@ -1,10 +1,16 @@
 package com.gpengtao;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * Hello world!
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		for (BigDecimal speed = new BigDecimal(8); speed.compareTo(new BigDecimal(11)) <= 0; speed = speed.add(new BigDecimal("0.1"))) {
+			BigDecimal time = new BigDecimal(60).divide(speed, 6, RoundingMode.HALF_UP);
+			System.out.println(speed + "  " + time.intValue() + "'" + time.subtract(new BigDecimal(time.intValue())).multiply(new BigDecimal(60)).setScale(0, RoundingMode.HALF_DOWN));
+		}
 	}
 }
